Pair plot changed to plot each groups bin averages, not each bin. Changed pair plot axes to begin at 0. Fixed error from commit c219e69 that lead to double logging of messages from interface.py.

process.py
@@ -6,13 +6,13 @@ Created on Wed Mar  6 12:42:28 2019
 """
 import system, math, pandas as pd, numpy as np, shapely.geometry as gm
 import pathlib as pl, re, warnings, decimal as dl, inspect
-import logger as lg
 from settings import settings as Sett
 from plot import plotter
 from system import store
 from scipy.ndimage import morphology as mp
 from skimage.morphology import skeletonize
 from skimage.filters import gaussian
+import logger as lg
 LAM_logger = lg.get_logger(__name__)
 
 def Create_Samples(PATHS):